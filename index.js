304f59d30a6fc2995144974b97601c3c9929fe6d
Tutorial-git <dk0540@digikull.com>
2023-10-27 17:25:26 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+
+db.on("error", (error) => {
+  console.error("MongoDB connection error:", error);
+});
+
+db.once("open", () => {
+  console.log("Connected to MongoDB");
+});
+
+db.once("close", () => {
+  console.log("Disconnected from MongoDB");
+});
+app.use("/api/users", require("./routes/authRoutes"));
+app.use("/api/users", require("./routes/taskRoutes"));
+app.use("/api", require("./routes/emailRoutes"));
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
+
+// username singharshdeep9039
+// password LhMp9JSjqEVXT3ND
